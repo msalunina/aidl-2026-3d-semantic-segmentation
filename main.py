d579afbf53a5b0706f973a0210047d3e80e6db86
olgaortega <ortegagelabert@gmail.com>
2026-01-26 13:46:41 +0100

    fix layer sizes, accuracy and naming

--- main.py
+++ main.py
@@ -78,14 +78,14 @@ if 1:
 
     # info item from the original dataset
     datasetItem = 1000
-    pos = full_train_dataset[datasetItem].pos.numpy()
+    pos = full_train_dataset[datasetItem].pos.cpu().numpy()
     label = full_train_dataset[datasetItem].y.item()
     fig = plt.figure(figsize=[7,7])
     ax = plt.axes(projection='3d')
     sc = ax.scatter(pos[:,0], pos[:,1], pos[:,2], c=pos[:,0] ,s=80, marker='o', cmap="viridis", alpha=0.7)
     ax.set_zlim3d(-1, 1)
     plt.title(f'Label: {label}')
-    plt.show
+    plt.show()
 
 
 # %%
@@ -97,10 +97,12 @@ def train_single_epoch(train_loader, network, optimizer, criterion):
     network.train()                             # Activate the train=True flag inside the model
 
     loss_history = []
-    acc_history = []
+    nCorrect = 0
+    nTotal = 0
     for batch in train_loader:
 
         batch = batch.to(device)
+        label = batch.y                                                 
         # Pointnet needs: [object, nPoints, coordinades] 
         # i.e. [32 object, 1024 points, 3 coordinates]: [batch_size, nPoints, 3]
         # Since batch.batch is [32x1024, 3] we have to split it into individual object points.
@@ -108,24 +110,31 @@ def train_single_epoch(train_loader, network, optimizer, criterion):
         # "to_dense_batch" will also add padding if not all objects have same number of points. In our case they have.
         # mask is a [batch_size, nPoints] boolean saying if an entry is actually a real point or padding
         BatchPointsCoords, _ = to_dense_batch(batch.pos, batch.batch)   
-        label = batch.y                                                 
 
         optimizer.zero_grad()                  # Set network gradients to 0
-        output = network(BatchPointsCoords)    # Forward batch through the network
-        loss = criterion(output, label)        # Compute loss
-        loss.backward()                        # Compute backpropagation
+        log_probs, _, _, feature_tnet_tensor, _ = network(BatchPointsCoords)    # Forward batch through the network      
+        # REGULARIZATION: force Tnet matrix to be orthogonal (TT^t = I)
+        # i.e. allow transforming the sapce but without distorting it
+        # The loss adds this term to be minimized: ||I-TT^t||
+        # It is a training constrain --> no need to be included in validation
+        TT = torch.bmm(feature_tnet_tensor, feature_tnet_tensor.transpose(2, 1))
+        I = torch.eye(TT.shape[-1], device=TT.device).unsqueeze(0).expand(TT.shape[0], -1, -1) # [64,64]->[1,64,64]->[batch,64,64]
+        reg_loss = torch.norm(I - TT) / TT.shape[0]                 # make reg_loss batch invariant (dividing by batch_size)
+        loss = criterion(log_probs, label) + 0.001 * reg_loss       # Compute loss: NLLLoss   
+        loss.backward()                                             # Compute backpropagation
         optimizer.step()    
         # Compute metrics
         loss_history.append(loss.item())         
-        prediction = output.argmax(dim=1)
-        nCorrect = (prediction == label).sum().item()   # .item() brings one single scalar to CPU
-        acc_history.append(nCorrect / len(label))
+        prediction = log_probs.argmax(dim=1)
+        batch_correct = (prediction == label).sum().item()          # .item() brings one single scalar to CPU
+        nCorrect = nCorrect + batch_correct
+        nTotal = nTotal + len(label)
         
     # Average across all batches    
-    train_loss = np.mean(loss_history) 
-    train_acc = np.mean(acc_history) 
+    train_loss_epoch = np.mean(loss_history) 
+    train_acc_epoch = nCorrect / nTotal
     
-    return train_loss, train_acc
+    return train_loss_epoch, train_acc_epoch
 # ----------------------------------------------------
 
 
@@ -138,66 +147,68 @@ def eval_single_epoch(data_loader, network, criterion):
         network.eval()                      # Dectivate the train=True flag inside the model
 
         loss_history = []
-        acc_history = []
+        nCorrect = 0
+        nTotal = 0
         for batch in data_loader:
             batch = batch.to(device)
 
-            BatchPointsCoords, _ = to_dense_batch(batch.pos, batch.batch)   
             label = batch.y 
+            BatchPointsCoords, _ = to_dense_batch(batch.pos, batch.batch)   
 
-            output = network(BatchPointsCoords)       # Forward batch through the network
-            loss = criterion(output, label)           # Compute loss
+            log_probs, _, _, _, _ = network(BatchPointsCoords)  # Forward batch through the network
+            loss = criterion(log_probs, label)                  # Compute loss
             # Compute metrics
             loss_history.append(loss.item())         
-            prediction = output.argmax(dim=1)
-            nCorrect = (prediction == label).sum().item()
-            acc_history.append(nCorrect / len(label))
+            prediction = log_probs.argmax(dim=1)
+            batch_correct = (prediction == label).sum().item()  # .item() brings one single scalar to CPU
+            nCorrect = nCorrect + batch_correct
+            nTotal = nTotal + len(label)
         
         # Average across all batches 
-        eval_loss = np.mean(loss_history)       
-        eval_acc = np.mean(acc_history) 
+        eval_loss_epoch = np.mean(loss_history)       
+        eval_acc_epoch = nCorrect / nTotal
     
-    return eval_loss, eval_acc
+    return eval_loss_epoch, eval_acc_epoch
 # ----------------------------------------------------
 
 # ----------------------------------------------------
 #    TRAINING LOOP (iterate on epochs)
 # ----------------------------------------------------
-def train_model(config, train_loader, val_loader, model, optimizer, criterion):
+def train_model(config, train_loader, val_loader, network, optimizer, criterion):
 
-    train_loss_epoch=[]
-    train_acc_epoch=[]
-    val_loss_epoch=[]
-    val_acc_epoch=[]
+    train_loss=[]
+    train_acc=[]
+    val_loss=[]
+    val_acc=[]
 
     for epoch in range(config["epochs"]):
-        train_loss, train_acc = train_single_epoch(train_loader, model, optimizer, criterion)
-        val_loss, val_acc = eval_single_epoch(val_loader, model, criterion)
+        train_loss_epoch, train_acc_epoch = train_single_epoch(train_loader, network, optimizer, criterion)
+        val_loss_epoch, val_acc_epoch = eval_single_epoch(val_loader, network, criterion)
         
-        train_loss_epoch.append(train_loss)
-        train_acc_epoch.append(train_acc)
-        val_loss_epoch.append(val_loss)
-        val_acc_epoch.append(val_acc)
+        train_loss.append(train_loss_epoch)
+        train_acc.append(train_acc_epoch)
+        val_loss.append(val_loss_epoch)
+        val_acc.append(val_acc_epoch)
 
-        print(f'Epoch: {epoch+1}/{config["epochs"]} | loss (train/val) = {train_loss:.4f}/{val_loss:.4f} | acc (train/val) ={train_acc:.2f}/{val_acc:.2f}')
+        print(f'Epoch: {epoch+1}/{config["epochs"]} | loss (train/val) = {train_loss_epoch:.4f}/{val_loss_epoch:.4f} | acc (train/val) ={train_acc_epoch:.2f}/{val_acc_epoch:.2f}')
     
     plt.figure(figsize=(10, 8))
     plt.subplot(2,1,1)
-    plt.plot(train_loss_epoch, label='train')
-    plt.plot(val_loss_epoch, label='val')
+    plt.plot(train_loss, label='train')
+    plt.plot(val_loss, label='val')
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
     plt.subplot(2,1,2)
-    plt.plot(train_acc_epoch, label='train')
-    plt.plot(val_acc_epoch, label='val')
+    plt.plot(train_acc, label='train')
+    plt.plot(val_acc, label='val')
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.legend()
 
     # plt.savefig("learning_curves.png")  # if server or remote instead of plt.show()
 
-    return model
+    return network
 
 
 # RUN ONLY IF EXECUTED AS MAIN
@@ -208,12 +219,12 @@ if __name__ == "__main__":
               "batch_size": 100}   
 
     # MODEL + OPTIMIZER + LOSS
-    model = ClassificationPointNet(num_classes=10).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=config["lr"])
+    network = ClassificationPointNet(num_classes=10).to(device)
+    optimizer = optim.Adam(network.parameters(), lr=config["lr"])
     criterion = nn.NLLLoss()
 
     # TRAINING LOOP
-    trained_model = train_model(config, train_loader, val_loader, model, optimizer, criterion)
+    trained_network = train_model(config, train_loader, val_loader, network, optimizer, criterion)
 
     plt.show()
 
--- pointNet.py
+++ pointNet.py
@@ -16,7 +16,6 @@ import numpy as np
 # nn.Conv1d(C → F, k=1)	 [B, C, N]	   [F, C, 1] (≡ [F, C])	  [F]	       [B, F, N]
 
 
-# %%    
 class TransformationNet(nn.Module):
     # For each pointset (object), predicts a transformation matrix (3x3 or 64x64) to make objects "canonical"
     # INPUT:  [batch, nPoints, 3]  -->  OUTPUT: [batch, 3, 3]      (channel=3)
@@ -30,22 +29,22 @@ class TransformationNet(nn.Module):
         # Ignoring batch dimension:_
         # For C=3, after transposing, the input for nn.Conv1d will be [3,nPoints]:
         #  W * x = x'  --> [64,3] * [3,nPoints] = [64,nPoints]
-        self.conv_1 = nn.Conv1d(input_dim, 64, 1)   # creates a weight matrix: [64,3]    --> output: [64,nPoints] 
-        self.conv_2 = nn.Conv1d(64, 128, 1)         # creates a weight matrix: [128,64]  --> output: [128,nPoints] 
-        self.conv_3 = nn.Conv1d(128, 256, 1)        # creates a weight matrix: [256,128] --> output: [256,nPoints] 
-
+        self.conv_1 = nn.Conv1d(input_dim, 64, 1)                   # weight matrix: [64,3]     --> output: [64,nPoints] 
+        self.conv_2 = nn.Conv1d(64, 128, 1)                         # weight matrix: [128,64]   --> output: [128,nPoints] 
+        self.conv_3 = nn.Conv1d(128, 1024, 1)                       # weight matrix: [1024,128] --> output: [1024,nPoints] 
         # Normalizes along features considering all objects and all points in the batch.
         # For instance: take feature #39 (out of 64, in this case) of each point and each opbject and normalize them.
-        # After normalizing, the collection of festures #39 will have mean=0 and std=1
+        # After normalizing, the collection of features #39 will have mean=0 and std=1
         self.bn_1 = nn.BatchNorm1d(64)  
         self.bn_2 = nn.BatchNorm1d(128)
-        self.bn_3 = nn.BatchNorm1d(256)
-        self.bn_4 = nn.BatchNorm1d(256)
-        self.bn_5 = nn.BatchNorm1d(128)
+        self.bn_3 = nn.BatchNorm1d(1024)
 
-        self.fc_1 = nn.Linear(256, 256)                             # weight matrix: [256,256] --> output: [256] 
-        self.fc_2 = nn.Linear(256, 128)                             # weight matrix: [128,256] --> output: [128] 
-        self.fc_3 = nn.Linear(128, self.output_dim*self.output_dim) # weight matrix: [3x3,128] --> output: [3x3] 
+        # Fully connected linear layers after maxPool (unlike nn.Conv1d, there is no nPoints dimension here)
+        self.fc_1 = nn.Linear(1024, 512)                            # weight matrix: [512,1024] --> output: [512] 
+        self.fc_2 = nn.Linear(512, 256)                             # weight matrix: [256,512]  --> output: [256] 
+        self.fc_3 = nn.Linear(256, self.output_dim*self.output_dim) # weight matrix: [3x3,256]  --> output: [3x3] 
+        self.bn_4 = nn.BatchNorm1d(512)
+        self.bn_5 = nn.BatchNorm1d(256)
 
     def forward(self, x):
         num_points = x.shape[1]
@@ -58,16 +57,16 @@ class TransformationNet(nn.Module):
         x = F.relu(self.bn_2(self.conv_2(x)))
         x = F.relu(self.bn_3(self.conv_3(x)))
 
-        # we have: [256,N] and we want the maximum value of each feature: --> [256,1]
+        # we have: [1024,N] and we want the maximum value of each feature: --> [1024,1]
         # For each feature, we choose the feature value of the point that maximises it 
-        # The output is a GLOBAL FEATURE VECTOR DESCRIBING THE OBJECT [256,1]
+        # The output is a GLOBAL FEATURE VECTOR DESCRIBING THE OBJECT [1024,1]
         # NOTA: we can't define maxPool1d in __init__ because num_points is only known at runtime.
         x = nn.MaxPool1d(num_points)(x)         # nn.MaxPool1d(kernel_size=num_points)
-        x = x.view(-1, 256)                     # "removes" the 1 dimension
+        x = x.view(-1, 1024)                     # "removes" the 1 dimension
 
-        x = F.relu(self.bn_4(self.fc_1(x)))     # from [256] to [256]: mixing
-        x = F.relu(self.bn_5(self.fc_2(x)))     # from [256] to [128]: compress
-        x = self.fc_3(x)                        # from [128] to [3x3=9] or [64x64=4096]
+        x = F.relu(self.bn_4(self.fc_1(x)))     # from [1024] to [512]: mixing
+        x = F.relu(self.bn_5(self.fc_2(x)))     # from [512] to [256]: compress
+        x = self.fc_3(x)                        # from [256] to [3x3=9] or [64x64=4096]
 
         # Reshape the vector into a matrix.
         # Add identity so that the transformation is only adding variatons.
@@ -86,20 +85,22 @@ class BasePointNet(nn.Module):
 
     def __init__(self, point_dimension):
         super().__init__()
-        self.input_transform = TransformationNet(input_dim=point_dimension, output_dim=point_dimension)
-        self.feature_transform = TransformationNet(input_dim=64, output_dim=64)
+        self.input_tnet = TransformationNet(input_dim=point_dimension, output_dim=point_dimension)
+        self.feature_tnet = TransformationNet(input_dim=64, output_dim=64)
 
+        # Shared MLP after input_transform (64,64)
         self.conv_1 = nn.Conv1d(point_dimension, 64, 1) # weight matrix: [64,3]    --> output: [64,nPoints] 
         self.conv_2 = nn.Conv1d(64, 64, 1)              # weight matrix: [64,64]   --> output: [64,nPoints] 
-        self.conv_3 = nn.Conv1d(64, 64, 1)              # weight matrix: [64,64]   --> output: [64,nPoints] 
-        self.conv_4 = nn.Conv1d(64, 128, 1)             # weight matrix: [128,64]  --> output: [128,nPoints] 
-        self.conv_5 = nn.Conv1d(128, 256, 1)            # weight matrix: [256,128] --> output: [256,nPoints] 
-
         self.bn_1 = nn.BatchNorm1d(64)
         self.bn_2 = nn.BatchNorm1d(64)
+
+        # Shared MLP after feature_transform (64,128,1024)  
+        self.conv_3 = nn.Conv1d(64, 64, 1)              # weight matrix: [64,64]    --> output: [64,nPoints]       
+        self.conv_4 = nn.Conv1d(64, 128, 1)             # weight matrix: [128,64]   --> output: [128,nPoints] 
+        self.conv_5 = nn.Conv1d(128, 1024, 1)           # weight matrix: [1024,128] --> output: [1024,nPoints] 
         self.bn_3 = nn.BatchNorm1d(64)
         self.bn_4 = nn.BatchNorm1d(128)
-        self.bn_5 = nn.BatchNorm1d(256)
+        self.bn_5 = nn.BatchNorm1d(1024)
 
 
     def forward(self, x, plot=False):
@@ -108,30 +109,32 @@ class BasePointNet(nn.Module):
         # INPUT x: [batch, nPoints, 3]
         # self.input_transform is a "function", it depends on x: 
         # - we pass x, we compute its matrix and then multiply (perform the transformation)                             
-        input_transform = self.input_transform(x)           # T-Net tensor:                 [batch, 3, 3]
-        x = torch.bmm(x, input_transform)                   # Batch matrix-matrix product   [batch, nPoints, 3]
-        
-        x = x.transpose(2, 1)                               # bcause nn.Conv1d needs input: [batch, 3, nPoints]                
-        tnet_out=x.cpu().detach().numpy()
-        x = F.relu(self.bn_1(self.conv_1(x)))               #                               [batch, 64, nPoints]
-        x = F.relu(self.bn_2(self.conv_2(x)))               #                               [batch, 64, nPoints]
-        x = x.transpose(2, 1)                               # because Tnet needs input:     [batch, nPoints, 64]       
-   
-        feature_transform = self.feature_transform(x)       # T-Net tensor                  [batch, 64, 64]
-        x = torch.bmm(x, feature_transform)                 # Batch matrix-matrix product   [batch, nPoints, 64]
-        
-        x = x.transpose(2, 1)                               # bcause nn.Conv1d needs input: [batch, 64, nPoints] 
-        x = F.relu(self.bn_3(self.conv_3(x)))               #                               [batch, 64, nPoints] 
-        x = F.relu(self.bn_4(self.conv_4(x)))               #                               [batch, 128, nPoints] 
-        x = F.relu(self.bn_5(self.conv_5(x)))               #                               [batch, 256, nPoints] 
-        x, ix = nn.MaxPool1d(num_points, return_indices=True)(x)  # max-pooling
-        x = x.view(-1, 256)                                 # global feature vector         [batch, 256]
+        input_tnet_tensor = self.input_tnet(x)        # T-Net tensor:                 [batch, 3, 3]
+        x = torch.bmm(x, input_tnet_tensor)                # Batch matrix-matrix product   [batch, nPoints, 3]
+        points_transformed = x
 
-        # x --> global feature vector describing the object
-        # ix --> nice for checking which point index contributed to the global feature
-        # tnet_out --> transfromed input points nice for visualization
-        # feature_transform matrix --> needed for loss (regularization)
-        return x, feature_transform, tnet_out, ix
+        x = x.transpose(2, 1)                                   # bcause nn.Conv1d needs input: [batch, 3, nPoints]                
+        x = F.relu(self.bn_1(self.conv_1(x)))                   #                               [batch, 64, nPoints]
+        x = F.relu(self.bn_2(self.conv_2(x)))                   #                               [batch, 64, nPoints]
+        x = x.transpose(2, 1)                                   # because Tnet needs input:     [batch, nPoints, 64]       
+   
+        feature_tnet_tensor= self.feature_tnet(x)    # T-Net tensor                  [batch, 64, 64]
+        x = torch.bmm(x, feature_tnet_tensor)              # Batch matrix-matrix product   [batch, nPoints, 64]
+        point_features = x
+
+        x = x.transpose(2, 1)                                   # bcause nn.Conv1d needs input: [batch, 64, nPoints] 
+        x = F.relu(self.bn_3(self.conv_3(x)))                   #                               [batch, 64, nPoints] 
+        x = F.relu(self.bn_4(self.conv_4(x)))                   #                               [batch, 128, nPoints] 
+        x = F.relu(self.bn_5(self.conv_5(x)))                   #                               [batch, 1024, nPoints] 
+        x, ix = nn.MaxPool1d(num_points, return_indices=True)(x)# max-pooling
+        global_features = x.view(-1, 1024)                      # global feature vector         [batch, 1024]
+
+        # global_features    --> global feature vector describing the object
+        # ix                 --> nice for checking which point index contributed to the global feature
+        # point_features     --> needed for segmentation
+        # feature_transform  --> matrix needed for loss (regularization)
+        # points_transformed --> transfromed input points nice for visualization
+        return global_features, ix, point_features, feature_tnet_tensor, points_transformed
 
 
     
@@ -142,21 +145,65 @@ class ClassificationPointNet(nn.Module):
         super().__init__()
         self.base_pointnet = BasePointNet(point_dimension=point_dimension)
 
-        self.fc_1 = nn.Linear(256, 128)                 # weight matrix: [128,256]        --> output: [128] 
-        self.fc_2 = nn.Linear(128, 64)                  # weight matrix: [64,128]         --> output: [64] 
-        self.fc_3 = nn.Linear(64, num_classes)          # weight matrix: [num_classes,64] --> output: [num_classes] 
-
-        self.bn_1 = nn.BatchNorm1d(128)
-        self.bn_2 = nn.BatchNorm1d(64)
-
+        # Fully connected linear layers (512,256,num_classes)
+        self.fc_1 = nn.Linear(1024, 512)                 # weight matrix: [512,1024]        --> output: [512] 
+        self.fc_2 = nn.Linear(512, 256)                  # weight matrix: [256,512]         --> output: [256] 
+        self.fc_3 = nn.Linear(256, num_classes)          # weight matrix: [num_classes,256] --> output: [num_classes] 
+        
+        self.bn_1 = nn.BatchNorm1d(512)
+        self.bn_2 = nn.BatchNorm1d(256)
+        
         self.dropout_1 = nn.Dropout(dropout)
 
     def forward(self, x):
-        x, feature_transform, tnet_out, ix_maxpool = self.base_pointnet(x)
+        global_features, ix_maxpool, point_features, feature_tnet_tensor, points_transformed = self.base_pointnet(x)
 
-        x = F.relu(self.bn_1(self.fc_1(x)))
+        x = F.relu(self.bn_1(self.fc_1(global_features)))
         x = F.relu(self.bn_2(self.fc_2(x)))
         x = self.dropout_1(x)
-        logits = F.log_softmax(self.fc_3(x), dim=1)
+        log_probs = F.log_softmax(self.fc_3(x), dim=1)
 
-        return logits#, feature_transform, tnet_out, ix_maxpool
+        return log_probs, ix_maxpool, point_features, feature_tnet_tensor, points_transformed
+
+
+
+class SegmentationPointNet(nn.Module):
+    def __init__(self, num_classes, dropout=0.3, point_dimension=3):
+        super().__init__()
+        self.base_pointnet = BasePointNet(point_dimension=point_dimension)
+
+        # Shared MLP (512,256,128)
+        self.conv_1 = nn.Conv1d(64+1024, 512, 1)          # weight matrix: [512,64+1024] --> output: [512,nPoints] 
+        self.conv_2 = nn.Conv1d(512, 256, 1)              # weight matrix: [256,512]     --> output: [256,nPoints] 
+        self.conv_3 = nn.Conv1d(256, 128, 1)              # weight matrix: [128,256]     --> output: [128,nPoints] 
+        self.bn_1 = nn.BatchNorm1d(512)
+        self.bn_2 = nn.BatchNorm1d(256)
+        self.bn_3 = nn.BatchNorm1d(128)
+
+        self.conv_4 = nn.Conv1d(128, 128, 1)              # weight matrix: [128,128]         --> output: [128,nPoints] 
+        self.conv_5 = nn.Conv1d(128, num_classes, 1)      # weight matrix: [num_classes,128] --> output: [num_classes,nPoints] 
+        self.bn_4 = nn.BatchNorm1d(128)
+
+        self.dropout_1 = nn.Dropout(dropout)
+
+
+    def forward(self, x):
+        global_features, ix_maxpool, point_features, feature_tnet_tensor, points_transformed = self.base_pointnet(x)
+        # [B,1024] global features vector
+        # [B,N,64] point features vector
+        nPoints = point_features.shape[1]
+        global_features = global_features.unsqueeze(1)              # [B,1,1024]
+        global_features = global_features.expand(-1, nPoints , -1)  # [B,N,1024]
+        x =  torch.cat([point_features, global_features], dim=2)
+
+        x = x.transpose(2, 1)                                       # [B, 1024+64, N]
+        x = F.relu(self.bn_1(self.conv_1(x)))
+        x = F.relu(self.bn_2(self.conv_2(x)))
+        x = F.relu(self.bn_3(self.conv_3(x)))
+
+        x = F.relu(self.bn_4(self.conv_4(x)))
+        x = self.dropout_1(x)
+        log_probs = F.log_softmax(self.conv_5(x), dim=1)
+
+        return log_probs, ix_maxpool, point_features, feature_tnet_tensor, points_transformed
+
